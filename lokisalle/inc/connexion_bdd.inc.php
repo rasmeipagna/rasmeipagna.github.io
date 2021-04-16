5b2ff48308ea83e1836ded8c395f05cfad97f0bf
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-04-16 13:21:58 +0200

    initial commit

--- lokisalle/inc/connexion_bdd.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$mysqli = new mysqli("cl1-sql21", "dgx20964", "lokisalle", "dgx20964");
-if($mysqli->connect_error)
-{
-	die("Oups ! Un probleme est survenu lors de la tentative de connexion a la BDD: ".$mysqli->connect_error);
-}
--- wp-content/themes/wp-simple/js/customizer.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('.wp-full-overlay-sidebar-content').prepend('<a href="http://www.nimbusthemes.com/wordpress-themes/simple/" class="button button-primary button-nimbus" target="_blank">Get Simple Pro</a>');
-});
